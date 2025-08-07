7d1bc6b0104ac5f3dc89a19a69177b909491233e
rgainju-neogenxai <rgainju@neogenxai.com>
2025-08-07 16:52:38 +0545

    flutter_rpg new project .23.

---
+++ flutter_rpg/lib/screens/home/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const StyledTitle('Your Characters'),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child:  Column(
+          children: [
+            const StyledText('Character List'),
+            const StyledHeading('Character list'),
+            const StyledTitle('Character list'),
+
+            FilledButton(
+              onPressed: () {}, 
+              child: const Text('Create New'),
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
